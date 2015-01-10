b7e9d09e45408011bec7cb77630c9a5df36d0c98
pallab-gain <pallab.gain@gmail.com>
2015-01-10 15:19:49 +0600

    finish activity on run

--- app/src/main/java/xerxes/networkspeedmonitor/MainActivity.java
+++ app/src/main/java/xerxes/networkspeedmonitor/MainActivity.java
@@ -4,45 +4,31 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.SeekBar;
 
-import networkmonitor.NetworkMonitorService;
 import repeater.AlarmManagerHandler;
 
 
 public class MainActivity extends ActionBarActivity {
     private AlarmManagerHandler alarmManagerHandler;
-    private SeekBar seekBar;
+//    private SeekBar seekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         alarmManagerHandler = AlarmManagerHandler.getInstance(getApplicationContext());
-        seekBar = (SeekBar)findViewById(R.id.seekBar);
-        seekBar.setOnSeekBarChangeListener(seekBarChangeListner);
+        MainActivity.this.finish();
     }
 
-    final SeekBar.OnSeekBarChangeListener seekBarChangeListner = new SeekBar.OnSeekBarChangeListener() {
-        @Override
-        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-            NetworkMonitorService.getInstance(getApplicationContext()).setFontSize((float)progress);
-        }
-
-        @Override
-        public void onStartTrackingTouch(SeekBar seekBar) {
-
-        }
-
-        @Override
-        public void onStopTrackingTouch(SeekBar seekBar) {
-
-        }
-    };
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
     @Override
